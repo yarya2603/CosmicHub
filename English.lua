6ea24aced592165e2c21183ae75e254cfcade813
yarya2603 <149476236+yarya2603@users.noreply.github.com>
2025-03-16 20:39:25 +0500

    Update English.lua

--- English.lua
+++ English.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "❄️ Mix HUB ❄️ | Scripts HUB",
+   Name = "❄️ Mix HUB ❄️ | Script HUB | English",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "❄️ Mix HUB ❄️",
    LoadingSubtitle = "by SKALA",
